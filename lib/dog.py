dd9d40ae2251aaee44d0bc3cb3328a2c8dfde480
evansochieng <evansochieng121@gmail.com>
2023-09-13 21:20:00 +0300

    complete attr and properties lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,34 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    
+    # initialize attribute
+    def __init__(self, name='Rex', breed='Pug'):
+        self.name = name
+        self.breed = breed
+        
+    # define name as a property
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if (type(name) == str) and (1 <= len(name) <= 25):
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            
+    # complile the getter and setter methods
+    name = property(get_name, set_name)
+    
+    # define breed as a property
+    def get_breed(self):
+        return self._breed
+    
+    def set_breed(self, breed):
+        if (breed in APPROVED_BREEDS):
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+            
+    # compile the getter and setter methods
+    breed = property(get_breed, set_breed)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,34 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    
+    # initialize attribute
+    def __init__(self, name='Evans', job='Admin'):
+        self.name = name
+        self.job = job
+        
+    # define name as a property
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if (type(name) == str) and (1 <= len(name) <= 25):
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            
+    # complile the getter and setter methods
+    name = property(get_name, set_name)
+    
+    # define job as a property
+    def get_job(self):
+        return self._job
+    
+    def set_job(self, job):
+        if (job in APPROVED_JOBS):
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+            
+    # compile the getter and setter methods
+    job = property(get_job, set_job)
